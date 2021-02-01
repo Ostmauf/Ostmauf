e5910edb9074f13a216c8e2f7a75fe85a0d79c37
Ostmauf <71626781+Ostmauf@users.noreply.github.com>
2021-02-01 10:14:21 +0200

    ny javascript övning

--- uppgifter/jsscript/javascript-hemmaupgt/brain.js
+++ uppgifter/jsscript/javascript-hemmaupgt/brain.js
@@ -1,24 +1,14 @@
-button1 = document.getElementById("button-1");
-button2 = document.getElementById("button-2");
-button3 = document.getElementById("button-3");
-button4 = document.getElementById("button-4");
-button5 = document.getElementById("button-5");
-button6 = document.getElementById("button-6");
-button7 = document.getElementById("button-7");
-button8 = document.getElementById("button-8");
-button9 = document.getElementById("button-9");
-button10 = document.getElementById("button-10");
-button11 = document.getElementById("button-11");
-button12 = document.getElementById("button-12");
-button13 = document.getElementById("button-13");
-button14 = document.getElementById("button-14");
-button15 = document.getElementById("button-15");
+let rubrik = document.getElementById("main-rubrik");
 
-function buttonManager() {
-    
-    switch() {
 
-        case1:
+
+function buttonManager(buttonSelect) {
+
+    switch(buttonSelect) {
+
+        case buttonSelect = 1:   
+            rubrik.style.fontSize = "2rem"
+        break;
     }
 }
 
