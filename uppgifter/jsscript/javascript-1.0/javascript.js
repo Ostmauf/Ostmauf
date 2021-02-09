1895f5337abf1c1eb1d491698a846634edf32b0f
Ostmauf <71626781+Ostmauf@users.noreply.github.com>
2021-02-09 11:17:50 +0200

    fixing

--- uppgifter/jsscript/javascript-1.0/javascript.js
+++ uppgifter/jsscript/javascript-1.0/javascript.js
@@ -1,7 +1,53 @@
-let d = new Date();
-let clock = document.getElementById("clock");
-let button1 = document.getElementById("button-1");
+let toSummer = document.getElementById("to-summer");
+let toBirth = document.getElementById("to-birth");
+let toChristmas = document.getElementById("to-christmas")
 
-//button1.addEventListener("click", getSec);
+//Setinterval
+setInterval(timeManager, 1000);
 
-console.log(d.getSeconds());
+//Funktioner
+function timeManager() {
+    
+    todaysDate = new Date()
+
+    //To summer
+    let sommerDate = new Date("2021-06-01");
+
+    dateDiffrenceManager(sommerDate, todaysDate)
+
+    
+
+    
+    /*//To birthday
+    let birthDate = new Date("2021-11-04");
+
+    
+
+    timeToBirth = timeToBirth.toFixed(0);
+
+    //To christmas
+    let christmasDate = new Date("2021-12-24");
+ 
+
+    timeToChristmas = timeToChristmas.toFixed(0);
+    
+    //Text
+    toSummer.innerHTML = "Det är " + timeToSummer + " sekunder till sommarlovet!";
+    toBirth.innerHTML = "Det är " + timeToBirth + " sekunder till min födelsedag!";
+    toChristmas.innerHTML = "Det är " + timeToChristmas + " sekunder till jul!";
+*/
+} 
+
+
+
+function dateDiffrenceManager(startDate, endDate) {
+
+    let timeConverter = (startDate.getTime() - endDate.getTime()) / 1000;
+    
+    timeConverter = timeConverter.toFixed(0);
+    
+    console.log(timeConverter);
+
+}
+
+
