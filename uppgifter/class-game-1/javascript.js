e32075c2782290273563685425497bca2f2f2d44
Ostmauf <71626781+Ostmauf@users.noreply.github.com>
2021-02-15 11:01:53 +0200

    fix

--- uppgifter/class-game-1/javascript.js
+++ uppgifter/class-game-1/javascript.js
@@ -1,21 +1,31 @@
-//Variables
-let balls = document.getElementsByClassName("ball");
+let balls = document.getElementsByClassName('ball');
 
-balls[0].addEventListener("click", ballClick)
+let i = 0;
+while(i < 10) {
 
-
-
-function ballClick() {
-
-    //Balls x and y position
-    /*
-    balls[0].style.left = ((window.innerWidth - 70) * Math.random()) + "px";
-    balls[0].style.top = ((window.innerHeight - 70) * Math.random()) + "px";
-    */ 
-
-   document.body.removeChild(balls);
+  balls[i].style.left = ((window.innerWidth - 80) * Math.random()) + "px";
+  balls[i].style.top = ((window.innerHeight - 80) * Math.random()) + "px";
+  balls[i].addEventListener('click', onBallClicked);
+  i++;
 
 }
+
+let startTime = new Date();
+console.log(startTime.getTime());
+
+function onBallClicked(event) {
+  document.body.removeChild(event.target);
+
+  console.log( document.getElementsByClassName('ball').length );
+
+  if(document.getElementsByClassName('ball').length == 0) {
+
+    let endTime = new Date();
+    console.log(endTime.getTime());
+
+  }
+}
+
 
 
 
