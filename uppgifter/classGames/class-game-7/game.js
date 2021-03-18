cd617c89d2e15885fd6c212fed376c4557effd4f
Ostmauf <71626781+Ostmauf@users.noreply.github.com>
2021-03-18 11:53:12 +0200

    continuing snake game

---
+++ uppgifter/classGames/class-game-7/game.js
@@ -0,0 +1,33 @@
+import { update as updateSnake, draw as drawSnake, snakeSpeed } from ".snake.js"
+
+let lastRenderTime = 0;
+
+function main(currentTime) {
+
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1 / snakeSpeed) return;
+
+    
+    lastRenderTime = currentTime;
+
+    update()
+    draw()
+
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+
+    updateSnake()
+
+}
+
+function draw() {
+
+    drawSnake()
+
+}
+
+//Info:  15:15 in video
---
+++ uppgifter/classGames/class-game-7/snake.js
@@ -0,0 +1,15 @@
+//Exports
+export const snakeSpeed = 2;
+
+//Export Functions
+ export function update() {
+
+
+
+}
+
+export function draw() {
+
+
+
+}
