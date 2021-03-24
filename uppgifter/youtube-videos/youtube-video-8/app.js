303975539f1864917cdb3b8f1713f0c1ed8a27fd
Ostmauf <71626781+Ostmauf@users.noreply.github.com>
2021-03-24 10:15:21 +0200

    Update app.js

--- uppgifter/youtube-videos/youtube-video-8/app.js
+++ uppgifter/youtube-videos/youtube-video-8/app.js
@@ -6,26 +6,63 @@ canvas.height = 500;
 const keys = [];
 
 const player = {
-    x: 0,
-    y: 0,
+    x: 200,
+    y: 300,
     width: 40,
     height: 72,
     frameX: 0,
-    frameY: 0,
+    frameY: 1,
     speed: 9,
     moving: false
 };
 
 const playerSprite = new Image();
-playerSprite.src = "chewie.png"
+playerSprite.src = "chewie.png";
 const background = new Image();
 background.src = "background.png";
 
+function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
+    ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+}
+
 function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    webkitRequestAnimationFrame(animate);
+    drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
+    movePlayer();
+    requestAnimationFrame(animate);
 }
 
 animate();
 
-//12:09
+window.addEventListener("keydown", function(e) {
+    keys[e.keyCode] = true;
+})
+
+window.addEventListener("keyup", function(e) {
+    delete keys[e.keyCode];
+})
+
+function movePlayer() {
+    if (keys[38] && player.y > 100) {
+        player.y -= player.speed;
+        player.frameY = 3;
+    }
+
+    if (keys[37] && player.x > 0) {
+        player.x -= player.speed;
+        player.frameY = 1;
+    } 
+
+    if (keys[40] && player.y < canvas.height - player.height) {
+        player.y += player.speed;
+        player.frameY = 0;
+    } 
+
+    if (keys[39] && player.x < canvas.width - player.width) {
+        player.x += player.speed;
+        player.frameY = 2;
+    } 
+}
+
+//26:47
